60494e50f63dddc41e9cd34781ef7480093a3d1c
Dawn Gay <dawngay@Dawns-iMac.local>
2015-04-19 13:22:55 -0500

    adventure v1

---
+++ adventure.rb
@@ -0,0 +1,70 @@
+class Hero
+  attr_accessor :name, :strength, :weapon
+  
+  def initialize (name, strength, weapon)
+    @name = name
+    @strength = strength
+    @weapon = weapon
+  end
+
+  def move
+  end
+
+  def battle
+  end
+
+  def power
+  end
+
+  def heal
+  end
+
+end
+
+class Path
+  attr_accessor :name, :enemy_num, :weapon_num, :health_num
+  
+  def initialize(name)
+    @name = name
+    @enemy_num = generate
+    @weapon_num = generate
+    @health_num = generate
+  end
+  
+  def generate()
+    @item = rand(10)
+  end
+  
+end
+
+class Enemy
+end
+
+class Weapon
+end
+
+class Health
+end
+
+class Adventure
+  def start
+    
+    
+    loop rand(10)
+    Path.new
+    
+    
+  end
+  
+  
+  def generate()
+    @item = rand(10)
+  end
+
+end
+
+
+newpath = Path.new("road")
+puts "There are #{newpath.enemy_num} enemies"
+puts "There are #{newpath.weapon_num} weapons"
+puts "There are #{newpath.health_num} health"
